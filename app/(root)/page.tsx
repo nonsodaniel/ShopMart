077b06b0ecf8b2a6e8001de83afb8edc585c2ade
nonsodaniel <nonsodaniel07@gmail.com>
2023-12-06 02:18:25 +0100

    Feat: Created global modal

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,16 @@
+"use client";
+import Modal from "@/components/ui/modal";
 import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div className="p-4">
+      <Modal
+        title="A sample modal"
+        description="This is my first sample modal"
+        isOpen
+        onClose={() => null}
+      />
       <UserButton afterSignOutUrl="/" />
     </div>
   );
---
+++ components/ui/modal.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+
+interface ModalProps {
+  title: string;
+  description: string;
+  isOpen: boolean;
+  onClose: () => void;
+  children?: React.ReactNode;
+}
+
+const Modal = ({
+  title,
+  description,
+  isOpen,
+  onClose,
+  children,
+}: ModalProps) => {
+  const onChange = (open: boolean) => {
+    if (!open) {
+      onClose();
+    }
+  };
+  return (
+    <Dialog open={isOpen} onOpenChange={onChange}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>{title}</DialogTitle>
+          <DialogDescription>{description}</DialogDescription>
+        </DialogHeader>
+        <div>{children}</div>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default Modal;
