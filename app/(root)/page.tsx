2f48f98b112965aa7a75b061548bbc206f769208
nonsodaniel <nonsodaniel07@gmail.com>
2023-12-06 02:00:13 +0100

    Feat: Implemented app authentication

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,9 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hello
-    </main>
+    <div className="p-4">
+      <UserButton afterSignOutUrl="/" />
+    </div>
   );
 }
